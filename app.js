e8bf1d7263c6dcc6769e3a53f1de44848ab19bde
meshnjihia <meshworks7@gmail.com>
2023-04-25 08:33:37 +0300

    added the variables required for the project

---
+++ app.js
@@ -0,0 +1,29 @@
+let originalStory;
+
+
+// original story
+/*
+
+
+
+
+
+*/
+
+// user inputs
+
+let userInputs = [];
+
+let questionArray = [''];
+
+let questions
+
+for (let i = questions; i >= 0; i--) {
+    console.log(i)
+    
+}
+
+
+// record the number of replacements made by the user
+
+let numberOfQuestions = 0;
